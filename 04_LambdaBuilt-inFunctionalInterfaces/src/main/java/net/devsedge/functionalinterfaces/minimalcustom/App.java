330892e69a4ec27ef1578568a00c198ac54db59e
Luigi Cortese <luigi.cortese00@gmail.com>
2015-11-24 11:45:56 +0100

    Added minimal custom functional interface

---
+++ 04_LambdaBuilt-inFunctionalInterfaces/src/main/java/net/devsedge/functionalinterfaces/minimalcustom/App.java
@@ -0,0 +1,26 @@
+package net.devsedge.functionalinterfaces.minimalcustom;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println(foo(12,12,(a,b)->a*b));
+		
+	}
+	
+	
+	static int foo(int a,int b,MathOperation m){
+		return m.calculate(a,b);
+	}
+
+}
+
+/**
+ * This is a custom functional interface, it represents
+ * a mathematical operation accepting two integers and returning one
+ * @author Mui
+ *
+ */
+interface MathOperation{
+	int calculate(int a,int b);
+}
