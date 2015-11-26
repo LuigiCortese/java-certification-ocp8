da178efe40edaaffd47c39268738d402787e11de
Luigi Cortese <luigi.cortese00@gmail.com>
2015-11-26 10:56:13 +0100

    Switched to "try with resources" statement

--- 11_BuildingDatabaseApplicationsWithJDBC/src/main/java/net/devsedge/jdbc/basics/App.java
+++ 11_BuildingDatabaseApplicationsWithJDBC/src/main/java/net/devsedge/jdbc/basics/App.java
@@ -13,31 +13,34 @@ import java.sql.Statement;
  */
 public class App {
 
-	public static void main(String[] args) throws SQLException {
-		
-		// Getting a Connection from DriverManager 
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "user", "password");
-		
-		// Getting a Statement from Connection
-		Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-		
-		// Getting a ResultSet from Statement
-		ResultSet res = stat.executeQuery("SELECT * FROM employee");
-		
-		// Iterating ResultSet
-		while (res.next()) {
-			System.out.print(res.getString(1) + " ");
-			System.out.print(res.getString(2) + " ");
-			System.out.print(res.getString(3) + " ");
-			System.out.println(res.getString(4));
-		}
-		
-		// Getting a ResultSetMetaData from ResultSet
-		ResultSetMetaData meta = res.getMetaData();
-		
-		// Reading ResultSetMetaData
-		System.out.println(meta.getColumnCount());
+	public static void main(String[] args) {
 
+		try (	// Getting a Connection from DriverManager 
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "user", "password");
+
+				// Getting a Statement from Connection
+				Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+				// Getting a ResultSet from Statement
+				ResultSet res = stat.executeQuery("SELECT * FROM employee");) {
+
+			// Iterating ResultSet
+			while (res.next()) {
+				System.out.print(res.getString(1) + " ");
+				System.out.print(res.getString(2) + " ");
+				System.out.print(res.getString(3) + " ");
+				System.out.println(res.getString(4));
+			}
+
+			// Getting a ResultSetMetaData from ResultSet
+			ResultSetMetaData meta = res.getMetaData();
+
+			// Reading ResultSetMetaData
+			System.out.println(meta.getColumnCount());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
