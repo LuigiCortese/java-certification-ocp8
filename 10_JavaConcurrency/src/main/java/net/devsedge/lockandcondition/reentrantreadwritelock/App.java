ed02bd2b3933aa7b4c8e08bfcf8767688add5c88
Luigi Cortese <luigi.cortese00@gmail.com>
2015-11-28 17:59:25 +0100

    ReentrantReadWriteLock example added

--- 10_JavaConcurrency/src/main/java/net/devsedge/lockandcondition/reentrantreadwritelock/App.java
+++ 10_JavaConcurrency/src/main/java/net/devsedge/lockandcondition/reentrantreadwritelock/App.java
@@ -1,4 +1,11 @@
 package net.devsedge.lockandcondition.reentrantreadwritelock;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import net.devsedge.NameUtil;
+import net.devsedge.Util;
+
 /**
  * 
  * @author Luigi Cortese
@@ -6,9 +13,69 @@ package net.devsedge.lockandcondition.reentrantreadwritelock;
  */
 public class App {
 
+	static NameUtil nameUtil;
+	static ReentrantReadWriteLock lock;
+	
+	static{
+		nameUtil=new NameUtil(20);
+		lock=new ReentrantReadWriteLock();
+	}
+	
 	public static void main(String[] args) {
-		// TODO: reentrantreadwritelock example
-
+		
+		Util.startTimer(10);
+		
+		new Thread(new Reader()).start();
+		new Thread(new Reader()).start();
+		new Thread(new Reader()).start();
+		new Thread(new Writer()).start();
+		
 	}
 
+}
+
+class Reader implements Runnable{
+	
+	private String name=App.nameUtil.getName();
+	
+	@Override
+	public void run() {
+		System.out.println(name+": waiting for R lock");
+		App.lock.readLock().lock();
+		System.out.println(name+": got R lock, reading");
+		
+		try { 
+			Thread.sleep(ThreadLocalRandom.current().nextLong(2000,6000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			App.lock.readLock().unlock();
+			System.out.println(name+": releasing R lock");
+		}
+		
+		System.out.println(name+": exiting");
+	}
+}
+
+class Writer implements Runnable{
+	
+	private String name=App.nameUtil.getName();
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(name+": waiting for RW lock");
+		App.lock.writeLock().lock();
+		System.out.println(name+": got RW lock, writing");
+		
+		try { 
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			App.lock.writeLock().unlock();
+			System.out.println(name+": releasing WR lock");
+		}
+		
+	}
 }
