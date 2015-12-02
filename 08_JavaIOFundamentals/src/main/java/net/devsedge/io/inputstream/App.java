a5dca3ad6b12bf48f1df7b1a22b7b2b30a36028b
Luigi Cortese <luigi.cortese00@gmail.com>
2015-12-02 23:58:01 +0100

    InputStream example

---
+++ 08_JavaIOFundamentals/src/main/java/net/devsedge/io/inputstream/App.java
@@ -0,0 +1,14 @@
+package net.devsedge.io.inputstream;
+/**
+ * 
+ * @author Luigi Cortese
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
