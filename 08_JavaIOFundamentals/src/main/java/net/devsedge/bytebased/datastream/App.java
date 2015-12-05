d8a8adba9f788abc2ee361a8374261b6b560e12d
Luigi Cortese <luigi.cortese00@gmail.com>
2015-12-05 23:44:16 +0100

    Data[Input|Output]Stream example completed

--- 08_JavaIOFundamentals/src/main/java/net/devsedge/bytebased/datastream/App.java
+++ 08_JavaIOFundamentals/src/main/java/net/devsedge/bytebased/datastream/App.java
@@ -1,5 +1,10 @@
 package net.devsedge.bytebased.datastream;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 /**
  * 
  * @author Luigi Cortese
@@ -8,8 +13,35 @@ package net.devsedge.bytebased.datastream;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * Writing a file
+		 */
+		
+		try(
+				FileOutputStream fos = new FileOutputStream("src/main/java/net/devsedge/bytebased/datastream/foo.txt");
+				DataOutputStream dos = new DataOutputStream(fos)){
+			dos.writeLong(123L);
+			dos.writeBoolean(false);
+			dos.writeUTF("UTF String");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		/*
+		 * Reading a file
+		 */
 
+		try(
+				FileInputStream fis = new FileInputStream("src/main/java/net/devsedge/bytebased/datastream/foo.txt");
+				DataInputStream dis = new DataInputStream(fis)){
+			System.out.println(dis.readLong());
+			System.out.println(dis.readBoolean());
+			System.out.println(dis.readUTF());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
