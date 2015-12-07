bb1de83d9f5fb225a4dd20f19aa5ff5af99ae0d8
Luigi Cortese <luigi.cortese00@gmail.com>
2015-12-07 18:32:17 +0100

    File creation example added

---
+++ 09_JavaFileIO-NIO2/src/main/java/net/devsedge/files/getwriter/App.java
@@ -0,0 +1,38 @@
+package net.devsedge.files.getwriter;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * 
+ * @author Luigi Cortese
+ *
+ */
+public class App {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Creating a simple file with Path Api
+		 */
+		
+		Path file = Paths.get("src/main/java","net/devsedge/files/getwriter","foo.txt");
+		
+		try(BufferedWriter bw = Files.newBufferedWriter(
+				file, 
+				Charset.forName("UTF-8"),
+				new OpenOption[] {StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE})){
+		
+		bw.write("This text file is created using Path API");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
